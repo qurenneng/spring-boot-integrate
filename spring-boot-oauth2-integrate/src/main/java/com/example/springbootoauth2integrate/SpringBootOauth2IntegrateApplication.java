9c89eb94d29a25d88779f50b19eb6ce852e6f895
qurenneng <1796789910@qq.com>
2021-04-21 22:17:26 +0800

    OAuth2

---
+++ spring-boot-oauth2-integrate/src/main/java/com/example/springbootoauth2integrate/SpringBootOauth2IntegrateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth2integrate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOauth2IntegrateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOauth2IntegrateApplication.class, args);
+    }
+
+}
---
+++ spring-boot-oauth2-integrate/src/test/java/com/example/springbootoauth2integrate/SpringBootOauth2IntegrateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootoauth2integrate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOauth2IntegrateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
