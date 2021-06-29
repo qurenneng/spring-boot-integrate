e2e5ce3cc26c9f2c374b6b5d29a7a8dbd3ab152d
qurenneng <1796789910@qq.com>
2021-06-29 22:00:35 +0800

    增加 redisson 代码

---
+++ spring-boot-redisson-integrat/src/main/java/com/example/springbootredissonintegrat/SpringBootRedissonIntegratApplication.java
@@ -0,0 +1,16 @@
+package com.example.springbootredissonintegrat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 增加 redisson 集成。
+ */
+@SpringBootApplication
+public class SpringBootRedissonIntegratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRedissonIntegratApplication.class, args);
+    }
+
+}
---
+++ spring-boot-redisson-integrat/src/test/java/com/example/springbootredissonintegrat/SpringBootRedissonIntegratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootredissonintegrat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRedissonIntegratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
