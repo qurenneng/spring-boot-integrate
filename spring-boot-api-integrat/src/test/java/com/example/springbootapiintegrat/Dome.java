179c1790dfd463779b38d79cc7243af3037c7497
qurenneng <1796789910@qq.com>
2021-07-28 14:30:20 +0800

    demo代码

--- spring-boot-api-integrat/src/test/java/com/example/springbootapiintegrat/Dome.java
+++ spring-boot-api-integrat/src/test/java/com/example/springbootapiintegrat/Dome.java
@@ -12,13 +12,46 @@ import java.util.stream.Collectors;
 public class Dome {
 
     public static void main(String[] args) {
-
-        int num = 11;
-
-        if(num < 10 || num > 200){
-            System.out.println("41111");
-        }else{
-            System.out.println("42222");
+        int[] nums = new int[4];
+        nums[0] = 2;
+        nums[1] = 7;
+        nums[2] = 11;
+        nums[3] = 15;
+        int target = 9;
+        int[] ints = twoSums(nums, target);
+        for(int i=0;i<ints.length;i++){
+            System.out.print(ints[i]);
         }
+    }
+
+    public static int[] twoSums(int[] nums, int target) {
+        int[] sum = new int[2];
+        for(int i=0;i<nums.length;i++){
+            for(int j=i+1;j<nums.length;j++){
+                if(nums[i]+nums[j] == target){
+                    sum[0]=i;
+                    sum[1]=j;
+                    return  sum;
+                }
+            }
+        }
+        return  sum;
+    }
+
+
+    public static int[] twoSum(int[] nums, int target) {
+        int[] sum = new int[2];
+        Map<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<nums.length;i++) {
+            //如果目标值减去数组中得到的值存在map中那么代表，相加等于 target
+            if (map.containsKey(target - nums[i])) {
+                sum[0] = map.get(target - nums[i]);
+                sum[1] = i;
+            }else{
+                map.put(nums[i],i);
+            }
+
+        }
+        return  sum;
     }
 }
